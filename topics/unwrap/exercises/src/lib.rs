f38be69ad384f51b9a16cb085027dbf11fa0f84c
Iyke <eokorie1911@gmail.com>
2026-02-19 18:23:18 +0100

    unwrap and expect i rustn

--- topics/unwrap/exercises/src/lib.rs
+++ topics/unwrap/exercises/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn parse_and_add(a: &str, b: &str) -> u32 {
-    todo!();
+  a.parse::<u32>().expect("Failed to parse variable") + b.parse::<u32>().expect("Failed to parse variable")
 }
 
 pub fn unwrap_and_add(x: Option<u32>, y: Option<u32>) -> u32 {
-    todo!();
+    x.unwrap() + y.unwrap()
 }
